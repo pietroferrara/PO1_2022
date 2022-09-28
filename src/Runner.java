d79f609c573f67cb46caf75eacf50e3d1b528b3c
Pietro Ferrara <pietro.ferrara@unive.it>
2022-09-28 12:11:43 +0200

    lecture 2

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,16 @@
 public class Runner {
+
+    int x;
+
     public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello "+name);
+        //String name = args[0];
+        //System.out.println("Hello "+name);
+    }
+    public int foo() {
+        return increment(x);
+    }
+
+    int increment(int i) {
+        return i+1;
     }
 }
